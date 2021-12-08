343cf35d7ed2fdd60a86df0c2d1402d59576879a
Simon Martin <smartin@scottlogic.com>
2021-12-08 11:47:30 +0000

    Day8 (#3)
    * Add template and day 7 puzzles solutions
    
    * Day 8 puzzle 2 in progress
    
    * Day 8 puzzle 2 complete
    
    * Day 8 puzzle 2 complete
    
    Co-authored-by: Simon Martin <havvoric@gmail.com>

---
+++ d8p1/src/lib.rs
@@ -0,0 +1,84 @@
+use std::cmp::{max, min};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+pub fn load(filename: &str) -> Solution {
+    let file = File::open(filename).unwrap();
+
+    let reader = BufReader::new(file);
+
+    let mut solution = Solution::new();
+    for line in reader.lines() {
+        let l = line
+            .unwrap()
+            .split("|")
+            .take(2)
+            .map(|v| Line::from_str(v).unwrap().to_owned())
+            .collect::<Vec<_>>();
+        
+        println!("{:?} => {:?}", l[0], l[1]);
+        solution.add(l.get(0).unwrap().to_owned(), l.get(1).unwrap().to_owned());
+    }
+
+    solution
+}
+
+#[derive(Debug)]
+pub struct Solution {
+    data: HashMap<Line, Line>,
+    answer: i64
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub struct Line {
+    positions: Vec<String>
+}
+
+impl Solution {
+    fn new() -> Self {
+        Self {data: HashMap::new(), answer: 0i64}
+    }
+
+    fn add(&mut self, signal: Line, output: Line) {
+        self.data.insert(signal, output);
+    }
+
+    pub fn analyse(&mut self) {
+        self.answer = 0;
+        for (_input, output) in &self.data {
+            for single in output.entry() {
+            self.answer += match single.len() {
+                2 => 1, // 1 only digit with 2 segments
+                3 => 1, // 7 only digit with 3 segments
+                4 => 1, // 4 only digit with 4 segments
+                7 => 1, // 8 only digit with 7 segments
+                _ => 0
+            }
+        }
+        }
+    }
+
+    pub fn answer(&self) -> i64 {
+        self.answer as i64
+    }
+}
+
+impl Line {
+    fn entry(&self) -> impl Iterator<Item=&String> + '_ {
+        self.positions.iter()
+    }
+}
+
+impl FromStr for Line {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let positions = s.trim().split_whitespace().map(|v| v.to_string()).collect::<Vec<_>>();
+
+        Ok(Line { positions })
+    }
+
+}
---
+++ d8p2/src/lib.rs
@@ -0,0 +1,171 @@
+use std::cmp::{max, min};
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+use itertools::Itertools;
+
+pub fn load(filename: &str) -> Solution {
+    let file = File::open(filename).unwrap();
+
+    let reader = BufReader::new(file);
+
+    let mut solution = Solution::new();
+    for line in reader.lines() {
+        let l = line
+            .unwrap()
+            .split("|")
+            .take(2)
+            .map(|v| Line::from_str(v).unwrap().to_owned())
+            .collect::<Vec<_>>();
+
+        println!("{:?} => {:?}", l[0], l[1]);
+        solution.add(l.get(0).unwrap().to_owned(), l.get(1).unwrap().to_owned());
+    }
+
+    solution
+}
+
+#[derive(Debug)]
+pub struct Solution {
+    data: Vec<(Line, Line)>,
+    answer: i64,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Line {
+    positions: Vec<Vec<char>>,
+}
+
+impl Solution {
+    fn new() -> Self {
+        Self {
+            data: Vec::new(),
+            answer: 0i64,
+        }
+    }
+
+    fn add(&mut self, signal: Line, output: Line) {
+        self.data.push((signal, output));
+    }
+
+    pub fn analyse(&mut self) {
+        self.answer = 0;
+        for (input, output) in &self.data {
+            let mut mapping = HashMap::new();
+            let mut r_mapping = HashMap::new();
+            for entry in input.entry() {
+                let value = match entry.len() {
+                    2 => 1,
+                    3 => 7,
+                    4 => 4,
+                    5 => -1,
+                    6 => -1,
+                    7 => 8,
+                    _ => panic!(),
+                };
+                if value >= 0 {
+                    mapping.entry(entry.clone()).or_insert(value);
+                    r_mapping.entry(value).or_insert(entry.clone());
+                }
+            }
+            let one = r_mapping
+                .get(&1)
+                .unwrap()
+                .iter()
+                .map(|v| v.to_owned())
+                .collect::<HashSet<_>>();
+            let four = r_mapping
+                .get(&4)
+                .unwrap()
+                .iter()
+                .map(|v| v.to_owned())
+                .collect::<HashSet<_>>();
+            println!("mapping: {:?}", mapping);
+
+            for entry in input.entry() {
+                if !mapping.contains_key(entry) {
+                    let this = entry.iter().map(|v| v.to_owned()).collect::<HashSet<_>>();
+                    let value = match entry.len() {
+                        6 => {
+                            if this.intersection(&four).count() == 4 {
+                                9
+                            } else if this.intersection(&one).count() == 2 {
+                                0
+                            } else {
+                                6
+                            }
+                        }
+                        5 => {
+                            if this.intersection(&one).count() == 2 {
+                                3
+                            } else if this.intersection(&four).count() == 2 {
+                                2
+                            } else {
+                                5
+                            }
+                        }
+                        _ => panic!(),
+                    };
+                    if value >= 0 {
+                        mapping.entry(entry.clone()).or_insert(value);
+                    }
+                }
+            }
+
+            println!("mapping: {:?}", mapping);
+
+            let mut line_score = 0;
+            for entry in output.entry() {
+                let digit = mapping.get(entry).unwrap();
+                line_score *= 10;
+                line_score += digit;
+            }
+            self.answer += line_score;
+        }
+        println!("{}", self.answer);
+    }
+
+    /*
+    1 : [ ,  , c,  ,  , f,  ] = 2
+    7 : [a,  , c,  ,  , f,  ] = 3
+    4 : [ , b, c, d,  , f,  ] = 4
+
+    2 : [a,  , c, d, e,  , g] = 5
+    3 : [a,  , c, d,  , f, g] = 5
+    5 : [a, b,  , d,  , f, g] = 5
+
+    0 : [a, b, c,  , e, f, g] = 6
+    6 : [a, b,  , d, e, f, g] = 6
+    9 : [a, b, c, d,  , f, g] = 6
+
+    8 : [a, b, c, d, e, f, g] = 7
+        [8, 6, 8, 7, 4, 9, 7]
+     */
+
+    pub fn answer(&self) -> i64 {
+        self.answer as i64
+    }
+}
+
+impl Line {
+    fn entry(&self) -> impl Iterator<Item = &Vec<char>> + '_ {
+        self.positions.iter()
+    }
+}
+
+impl FromStr for Line {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let positions = s
+            .trim()
+            .split_whitespace()
+            .map(|v| v.chars().sorted().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        Ok(Line { positions })
+    }
+}
