29c0940f098a61a34150dfc2cf855e567a41ec90
Simon Martin <havvoric@gmail.com>
2021-12-14 06:52:39 +0000

    Day 14 complete

---
+++ d14p2/src/main.rs
@@ -0,0 +1,8 @@
+use d14p2::load;
+
+fn main() {
+    let mut solution = load("input.d14p1.full");
+    println!("solution: {:?}", solution);
+    solution.analyse();
+    println!("answer is {}", solution.answer());
+}
