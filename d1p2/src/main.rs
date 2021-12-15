04365832b73a00f8331b0f9f91097102a7f0a7cd
Simon Martin <smartin@scottlogic.com>
2021-12-15 13:48:45 +0000

    Day1p1: Add logger, fluent calculation (#12)

--- d1p2/src/main.rs
+++ d1p2/src/main.rs
@@ -1,20 +1,20 @@
 use std::fs::File;
-
 use std::io::{BufRead, BufReader};
 
-fn sum(values: &[Result<String, std::io::Error>]) -> i32 {
+use log::{debug, info};
+
+fn sum(values: &[i32]) -> i32 {
     let mut total = 0;
-    for v in values {
-        if let Ok(value) = v {
-            let reading: i32 = value.parse().unwrap();
-            total += reading;
-        }
+    for value in values {
+        total += value;
     }
     total
 }
 
 fn main() {
-    let filename = "input";
+    env_logger::init();
+
+    let filename = "input.d1p1.full";
 
     // Open the file in read-only mode (ignoring errors).
 
@@ -22,35 +22,25 @@ fn main() {
 
     let reader = BufReader::new(file);
 
-    let mut last: Option<i32> = None;
-    let mut count = 0;
-
-    let values: Vec<_> = reader.lines().collect();
+    let values: Vec<_> = reader
+        .lines()
+        .filter_map(|v| v.map(|v| v.parse::<i32>().unwrap()).ok())
+        .collect();
     let sums = values.windows(3).map(sum).collect::<Vec<_>>();
-    println!("{:?}", sums);
-    for sum in sums {
-        match last {
-            Some(v) if sum > v => count += 1,
-            _ => {}
-        }
-        last = Some(sum);
+    debug!("{:?}", sums);
+    let count = sums
+        .into_iter()
+        .fold(None, accumulate)
+        .map(|(_, count)| count)
+        .unwrap_or(0);
+    info!("Count = {}", count);
+}
+
+fn accumulate(acc: Option<(i32, i32)>, reading: i32) -> Option<(i32, i32)> {
+    debug!("reading: {}", reading);
+    match acc {
+        Some((last, count)) if reading > last => Some((reading, count + 1)),
+        Some((_last, count)) => Some((reading, count)),
+        None => Some((reading, 0)),
     }
-    /*
-
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        let reading: i32 = line.parse().unwrap();
-
-        match last {
-            Some(v) if reading > v => count += 1,
-            _ => {}
-        }
-        // Show the line and its number.
-
-        println!("{}. {}", index + 1, reading);
-        last = Some(reading);
-
-    }
-    */
-    println!("Count = {}", count);
 }
