1b720a6571750a6c3a9e14b551d42b2a4551854e
Simon Martin <smartin@scottlogic.com>
2021-12-15 15:54:04 +0000

    Day 3
    Add logging, fluent calculations and dedupe implementations

--- d3p2/src/main.rs
+++ d3p2/src/main.rs
@@ -2,10 +2,13 @@ use std::fs::File;
 
 use std::io::{BufRead, BufReader};
 
+use log::{debug, info};
 use std::collections::HashMap;
 
 fn main() {
-    let filename = "input";
+    env_logger::init();
+
+    let filename = "input.d3p1.full";
 
     // Open the file in read-only mode (ignoring errors).
 
@@ -16,108 +19,49 @@ fn main() {
     let data: Vec<String> = reader.lines().map(|v| v.unwrap()).collect();
     println!("{:?}", data);
 
-    let mut oxygen: Vec<String> = data.iter().map(|v| v.clone()).collect();
-    let mut index = 0;
-    while oxygen.len() > 1 {
-        let p = oxygen.iter().fold(HashMap::new(), |mut acc, v| {
-            *acc.entry(v.as_bytes()[index] as char).or_insert(0) += 1;
-            acc
-        });
-        let mut most_common_value = p
-            .iter()
-            .max_by(|a, b| a.1.cmp(&b.1))
-            .map(|(k, _v)| k)
-            .unwrap();
-        if p.get(&'0') != None && p.get(&'1') != None && p[&'0'] == p[&'1'] {
-            most_common_value = &'1';
-        }
-        println!("{:?} {}", p, most_common_value);
-        let next_oxygen: Vec<String> = oxygen
-            .iter()
-            .filter(|v| v.as_bytes()[index] as char == *most_common_value)
-            .map(|v| v.to_owned())
-            .collect();
-        println!("next: {} {:?}", index, next_oxygen);
-        oxygen = next_oxygen;
-        index += 1;
-    }
-    let mut co2: Vec<String> = data.iter().map(|v| v.clone()).collect();
-    let mut index = 0;
-    while co2.len() > 1 {
-        let p = co2.iter().fold(HashMap::new(), |mut acc, v| {
-            *acc.entry(v.as_bytes()[index] as char).or_insert(0) += 1;
-            acc
-        });
-        let mut least_common_value = p
-            .iter()
-            .min_by(|a, b| a.1.cmp(&b.1))
-            .map(|(k, _v)| k)
-            .unwrap();
-        if p.get(&'0') != None && p.get(&'1') != None && p[&'0'] == p[&'1'] {
-            least_common_value = &'0';
-        }
-        println!("{:?} {}", p, least_common_value);
-        let next_co2: Vec<String> = co2
-            .iter()
-            .filter(|v| v.as_bytes()[index] as char == *least_common_value)
-            .map(|v| v.to_owned())
-            .collect();
-        println!("next: {} {:?}", index, next_co2);
-        co2 = next_co2;
-        index += 1;
-    }
-    let oxygen = i32::from_str_radix(&oxygen.get(0).unwrap(), 2).unwrap();
-    let co2 = i32::from_str_radix(&co2.get(0).unwrap(), 2).unwrap();
+    let oxygen = calculate_rating(&data, |count_ones, total| {
+        count_ones >= (total - count_ones)
+    });
+    info!("oxygen = {}", oxygen);
+    let co2 = calculate_rating(&data, |count_ones, total| count_ones < (total - count_ones));
+    info!("co2 = {}", co2);
+    info!("{} x {} = {}", oxygen, co2, oxygen * co2);
+}
 
-    println!("{} x {} = {}", oxygen, co2, oxygen * co2);
-    /*
-    let mut last: Option<i32> = None;
-    let mut count = 0;
-    let mut counts = HashMap::new();
-
-    for line in reader.lines() {
-        count += 1;
-        let line = line.unwrap();
-        let mut reading = u32::from_str_radix(&line, 2).unwrap();
-
-        println!("{} -> {}", line, reading);
-        let mut index = 0;
-        while reading > 0 {
-            let field = reading & 1;
-            println!("  {} {}", index, field);
-            *counts.entry(index).or_insert(0) += field;
-            reading >>= 1;
-            index += 1;
-        }
-
-        /*
-        let reading: i32 = line.parse().unwrap();
-
-        match last {
-            Some(v) if reading > v => count += 1,
-            _ => {}
-        }
-        // Show the line and its number.
-
-        println!("{}. {}", index + 1, reading);
-        last = Some(reading);
-        */
-
-    }
-    println!("Count = {} {:?}", count, counts);
-
-    let mut epsilon = 0;
-    let mut gamma = 0;
-    for (k, v) in counts {
-        if v > (count >> 1) {
-          let value = 1 << k;
-          epsilon += value;
+fn calculate_rating(data: &Vec<String>, rule: impl Fn(usize, usize) -> bool) -> i64 {
+    let mut needle = "".to_string();
+    let mut last_match = data.len();
+    loop {
+        let mut probe = needle.clone();
+        probe.push('1');
+        let mut num_match = data.iter().filter(|v| (*v).starts_with(&probe)).count();
+        if rule(num_match, last_match) {
+            needle.push('1');
         } else {
-          let value = 1 << k;
-          gamma += value;
+            needle.push('0');
+            num_match = last_match - num_match;
         }
+        debug!(
+            "probe {}=>{}, num_match {} vs {}",
+            probe,
+            needle,
+            num_match,
+            last_match - num_match
+        );
+        match data.iter().filter(|v| (*v).starts_with(&needle)).count() {
+            1 => {
+                return data
+                    .iter()
+                    .filter(|v| (*v).starts_with(&needle))
+                    .fold(0, |_acc, v| {
+                        debug!("{}", v);
+                        i64::from_str_radix(v, 2).unwrap()
+                    })
+            }
+            0 => panic!(),
+            _ => {}
+        };
 
+        last_match = num_match;
     }
-    println!("total = {} x {} = {}", epsilon, gamma, epsilon * gamma);
-    */
 }
