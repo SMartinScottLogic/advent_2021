80f25cd72f4344aa8bce410adc5ccea812765f4b
Simon Martin <smartin@scottlogic.com>
2021-12-15 14:20:11 +0000

    Day 2: add logging, fluent calculations

--- d2p1/src/main.rs
+++ d2p1/src/main.rs
@@ -1,30 +1,57 @@
+use core::num::ParseIntError;
+use core::str::FromStr;
+use log::{debug, info};
 use std::fs::File;
-
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let filename = "input";
+    env_logger::init();
+    let filename = "input.d2p1.full";
 
     // Open the file in read-only mode (ignoring errors).
 
     let file = File::open(filename).unwrap();
 
     let reader = BufReader::new(file);
+    let position = reader
+        .lines()
+        .map(Result::unwrap)
+        .filter_map(|v| Instruction::from_str(&v).ok())
+        .fold((0, 0), accumulate);
+    debug!("position = {:?}", position);
+    info!("result: {}", position.0 * position.1);
+}
 
-    let mut position = (0, 0);
-
-    for line in reader.lines() {
-        let line = line.unwrap();
-        let mut s = line.split(" ");
-        let d = (s.next().unwrap(), s.next().unwrap().parse::<i32>().unwrap());
-        println!("{:?}", d);
-        match d {
-            ("forward", v) => position.0 += v,
-            ("down", v) => position.1 += v,
-            ("up", v) => position.1 -= v,
-            (o, _v) => panic!("Unexpected direction '{}'", o),
-        }
+fn accumulate(position: (i32, i32), instruction: Instruction) -> (i32, i32) {
+    use Instruction::*;
+    debug!("{:?}", instruction);
+    match instruction {
+        Forward(v) => (position.0 + v, position.1),
+        Down(v) => (position.0, position.1 + v),
+        Up(v) => (position.0, position.1 - v),
+        _ => position,
     }
-    println!("position = {:?}", position);
-    println!("result: {}", position.0 * position.1);
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+    None,
+}
+
+impl FromStr for Instruction {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim().split(" ").take(2).collect::<Vec<_>>();
+        let s = match &s[0..=1] {
+            ["forward", v] => Self::Forward(v.parse().unwrap()),
+            ["down", v] => Self::Down(v.parse().unwrap()),
+            ["up", v] => Self::Up(v.parse().unwrap()),
+            _ => Self::None,
+        };
+        Ok(s)
+    }
 }
