5182267fc55bb5a1d89c49b6b7a4f79230d32938
Simon Martin <havvoric@gmail.com>
2021-12-17 17:35:22 +0000

    Use info logging for answer

--- d17p2/src/main.rs
+++ d17p2/src/main.rs
@@ -1,13 +1,14 @@
 use anyhow::Result;
 use d17p2::load;
+use log::{debug,info};
 
 fn main() -> Result<()> {
     env_logger::init();
 
     let mut solution = load("input.d17p1.full")?;
-    println!("solution: {:?}", solution);
+    debug!("solution: {:?}", solution);
     solution.analyse();
-    println!("answer is {}", solution.answer()?);
+    info!("answer is {}", solution.answer()?);
 
     Ok(())
 }
