12defc28faf58702da223fb0e8897ffb567a2800
Simon Martin <smartin@scottlogic.com>
2021-12-24 20:28:18 +0000

    Add Xmas emoji to template main (#29)
    Co-authored-by: Simon Martin <havvoric@gmail.com>

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,13 +1,24 @@
 use anyhow::Result;
+use log::info;
 use template::load;
+use yansi::Paint;
 
 fn main() -> Result<()> {
     env_logger::init();
 
     let mut solution = load("input.full")?;
-    println!("solution: {:?}", solution);
+    info!(
+        "{}{}: {:?}",
+        Paint::masked("🎄 "),
+        Paint::bold(Paint::yellow("solution")),
+        solution
+    );
     solution.analyse();
-    println!("answer is {}", solution.answer()?);
+    info!(
+        "{}answer is {}",
+        Paint::masked("🎅 "),
+        Paint::bold(Paint::red(solution.answer()?))
+    );
 
     Ok(())
 }
